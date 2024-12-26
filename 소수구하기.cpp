eacb5992f32c67b0943aebc3c71a26a7d4832d71
hiden52 <zerorequiem23@gmail.com>
2024-12-26 15:48:39 +0900

    소수구하기

---
+++ 소수구하기.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> intList;
+	vector<int>::iterator itr;
+
+	for (int i = 1; i < 51; i++)
+	{
+		intList.push_back(i);
+		
+	}
+	for (int i : intList)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	for (int i = 1; i < intList.size(); i++)
+	{
+		for (int j = i + 1; j < intList.size();)
+		{
+			if ((intList[j] % intList[i]) == 0)
+			{
+				intList.erase(intList.begin() + j);
+			}
+			else
+			{
+				j++;
+			}
+		}
+	}
+	for (int i : intList)
+	{
+		cout << i << " ";
+	}
+
+	
+}
